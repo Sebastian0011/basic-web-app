11bb1fd33b4db86b046dca984b032b7ba42a7cf5
Sebastian0011 <sebastil@andrew.cmu.edu>
2023-09-15 14:27:08 -0400

    added andrew id query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,10 +1,16 @@
 export default function QueryProcessor(query: string): string {
-  if (query.toLowerCase().includes("shakespeare")) {
+  let queryString = query.toLowerCase()
+
+  if (queryString.includes("shakespeare")) {
     return (
       "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
       "English poet, playwright, and actor, widely regarded as the greatest " +
       "writer in the English language and the world's pre-eminent dramatist."
     );
+  } else if (queryString.includes("andrew id")) {
+    return (
+      "Your andrew id is hello!!"
+    )
   }
 
   return "";
